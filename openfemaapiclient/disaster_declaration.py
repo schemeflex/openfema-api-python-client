1168117cc998efc43cfd1e02cb98265ea0c534f6
taydon220 <taydon220@gmail.com>
2020-08-31 15:56:04 -0500

    Add datetime type hinting to match api 'date' type returns.

--- openfemaapiclient/disaster_declaration.py
+++ openfemaapiclient/disaster_declaration.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
@@ -7,7 +8,7 @@ class DataClassDisasterDeclaration:
     disasterNumber: int
     state: str
     declarationType: str
-    declarationDate: str  # FEMA api lists as date type. Ex) "declarationDate":"1965-09-10T04:00:00.000Z"
+    declarationDate: datetime  # Ex) "declarationDate":"1965-09-10T04:00:00.000Z"
     fyDeclared: int
     incidentType: str
     declarationTitle: str
@@ -15,14 +16,14 @@ class DataClassDisasterDeclaration:
     iaProgramDeclared: bool
     paProgramDeclared: bool
     hmProgramDeclared: bool
-    incidentBeginDate: str  # FEMA api lists as date type. Ex) "incidentBeginDate":"1966-03-22T05:00:00.000Z"
-    incidentEndDate: str  # FEMA api lists as date type. Ex) "incidentEndDate":"1965-09-10T04:00:00.000Z"
-    disasterCloseoutDate: str  # FEMA api lists as date type. Ex) "disasterCloseOutDate":"1967-08-24T04:00:00.000Z"
+    incidentBeginDate: datetime
+    incidentEndDate: datetime
+    disasterCloseoutDate: datetime
     fipsStateCode: str
     fipsCountyCode: str
     placeCode: str
     designatedArea: str
     declarationRequestNumber: str
-    lastRefresh: str  # FEMA api lists as date type. Ex) "lastRefresh":"2019-07-26T18:49:32.438Z"
+    lastRefresh: datetime
     hash: str
     id: str
