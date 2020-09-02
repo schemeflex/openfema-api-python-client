c2a39ad19d3975221320151e257572b7e3258f41
taydon220 <taydon220@gmail.com>
2020-09-02 09:34:29 -0500

    Create pa_applicant.py and pa_funded_project.py dataclasses.

--- openfemaapiclient/pa_funded_project.py
+++ openfemaapiclient/pa_funded_project.py
@@ -1,42 +1,30 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-
-def declaration_mapper(records):
-    declaration_datetime_fields = {"declarationDate", "incidentBeginDate", "incidentEndDate", "disasterCloseoutDate", "lastRefresh"}
-    results = []
-    for record in records:
-        mapped_result = {k: __to_datetime(v) if k in declaration_datetime_fields else v for k, v in record.items()}
-        results.append(DisasterDeclaration(**mapped_result))
-    return results
-
-
-def __to_datetime(value):
-    return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ") if value else None
+# Uses the pa funded projects details api endpoint.
 
 
 @dataclass(frozen=True)
 class DisasterDeclaration:
-    femaDeclarationString: str
     disasterNumber: int
+    pwNumber: int
+    applicationTitle: str
+    applicantId: str
+    county: str
+    countyCode: int
+    stateNumberCode: int
     state: str
-    declarationType: str
-    declarationDate: datetime  # Ex) "declarationDate":"1965-09-10T04:00:00.000Z"
-    fyDeclared: int
-    incidentType: str
-    declarationTitle: str
-    ihProgramDeclared: bool
-    iaProgramDeclared: bool
-    paProgramDeclared: bool
-    hmProgramDeclared: bool
-    incidentBeginDate: datetime
-    incidentEndDate: datetime
-    disasterCloseoutDate: datetime
-    fipsStateCode: str
-    fipsCountyCode: str
-    placeCode: str
-    designatedArea: str
-    declarationRequestNumber: str
+    projectAmount: float
+    stateCode: str
+    totalObligated: float
+    federalShareObligated: float
+    damageCategory: str
+    dcc: str
     lastRefresh: datetime
-    hash: str
     id: str
+    hash: str
+    projectSize: str
+    damageCategoryCode: str
+    declarationDate: datetime
+    incidentType: str
+    obligatedDate: datetime
